38c4aa6f28b16826c66054be8ca8d2878859532b
raoofha <raoofha@yahoo.com>
2016-11-20 15:57:41 +0330

    change another directory

--- workspace.sh
+++ workspace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-i3-msg 'append_layout /home/raoof/github/dotfiles/workspace.json'
+i3-msg 'append_layout ~/projects/dotfiles/workspace.json'
 chromium "http://localhost:4000" --new-window &
 xterm -e "cd $(pwd) && /bin/zsh" &
 xterm -e "cd $(pwd) && /bin/zsh" &
