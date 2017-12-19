7d40b49d6cc146544374c18409664011939c074a
Raoof <raoofha@yahoo.com>
2017-12-19 14:58:44 +0330

    add x11 autostart and ...

--- ws.sh
+++ ws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 i3-msg 'append_layout ~/projects/dotfiles/ws.json'
 chromium "http://localhost:3000" --new-window &
-xterm -e "cd $(pwd) && /bin/zsh" &
-xterm -e "cd $(pwd) && /bin/zsh" &
-xterm -e "cd $(pwd) && /bin/zsh" &
-xterm -e "cd $(pwd) && /bin/zsh" &
+xcwd | xargs urxvt -cd &
+xcwd | xargs urxvt -cd &
+xcwd | xargs urxvt -cd &
+xcwd | xargs urxvt -cd &
